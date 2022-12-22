764f403ce40f5b6bfd9226b92a37f8c51c98dfe3
Chanarongpimket <Chanarongpimket@gmail.com>
2022-12-22 23:44:49 +0700

    Part 3

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -33,5 +33,16 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = price
+    raise ArgumentError.new("Invalid ISBN") if @isbn.empty?
+    raise ArgumentError.new("Invalid price") if @price <= 0
+  end
+
+  def price_as_string
+    "$#{'%.2f' % @price}"
+  end
 end
